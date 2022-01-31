0fb92198406f4c5e0ceafc33ec7053444d1e7640
Krystiane Otis <oti19001@byui.edu>
2022-01-31 14:28:59 -0700

    more constants

--- contants.py
+++ contants.py
@@ -1,9 +1,12 @@
-#GAME WINDOW
-TILE_SIZE = 32
-COLS = 20
-MARGIN = 100
-SCREEN_WIDTH = TILE_SIZE * COLS
-SCREEN_HEIGHT = (TILE_SIZE * COLS) + MARGIN
+#frames per second
+CLOCK = pygame.time.Clock()
+FPS = 60
+
+#screen width and height
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 #define colors !!!these are just temp CHANGE THESE TO WHAT ARE NEEDED
 BLUE = (200, 255, 255)
@@ -11,3 +14,15 @@ LBLUE = (50, 255, 255)
 DBLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 PINK = (255, 86, 221)
+
+# load images
+PLAYER = pygame.image.load("Assets/temp_sun.png")
+
+#define font - if we want to have things written on the screen this is the font style and size
+FONT = pygame.font.SysFont('Bauhaus 93', 70)
+FONT_SCORE = pygame.font.SysFont('Bauhaus 93', 30)
+
+
+# load sounds and set volume
+ITEM_COLLECT = pygame.mixer.Sound("Assets/sounds/itemcollect.wav")
+ITEM_COLLECT.set_volume(0.5)

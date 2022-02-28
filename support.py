66ea86b68dd558339666b72f2ca53f6224bdcbae
thunderbioink <allealle0303@gmail.com>
2022-02-28 15:27:02 -0700

    support file added

---
+++ support.py
@@ -0,0 +1,12 @@
+from os import walk
+import pygame
+
+def import_fodler(path):
+    surface_list = []
+    
+    for _, __, img_files:
+        
+        full_path = path + '/' + image
+        image_surf = pygame.image.load(full_path).convert_alpha()
+        surface_list.append(image_surf)
+        return surface_list

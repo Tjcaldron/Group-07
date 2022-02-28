641bbeb39738175b159855657b547c776acb5866
Mosiah Hazen <haz17005@byui.edu>
2022-02-28 15:29:46 -0700

    pulling plus new class

---
+++ power_up.py
@@ -0,0 +1,8 @@
+import pygame
+from support import import_folder
+
+class Power_Up(pygame.sprite.Sprite):
+    def __init__(self, pos, size):
+        super().__init__()
+        self.image
+        self.rect

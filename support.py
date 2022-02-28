a84b2b07c1938c283c4793e7968902c861469a0b
Mosiah Hazen <haz17005@byui.edu>
2022-02-28 15:33:50 -0700

    support

--- support.py
+++ support.py
@@ -4,9 +4,10 @@ import pygame
 def import_fodler(path):
     surface_list = []
     
-    for _, __, img_files:
-        
-        full_path = path + '/' + image
-        image_surf = pygame.image.load(full_path).convert_alpha()
-        surface_list.append(image_surf)
+    for _, __, img_files in walk(path):
+        for image in img_files:
+            full_path = path + '/' + image
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surf)
+            
         return surface_list

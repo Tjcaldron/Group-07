75d7035646c164e2b5352accdeee026904e02e7d
thunderbioink <allealle0303@gmail.com>
2022-03-02 12:18:11 -0700

    background game audio in main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pygame, sys
 from settings import *
-from level import Level
+# from level import Level
 # from game_data import level_0
-
+from pygame import mixer
+pygame.init()
+# Mixer needs to be called for sound
+mixer.init()
+mixer.music.load("sound\english_bckgnd_lofi.wav")
+mixer.music.play(-1)
 # Pygame setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width,screen_height))

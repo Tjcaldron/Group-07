1a9fbb41f974e3121cd85bdbcdf4df51a79920bc
Mosiah Hazen <haz17005@byui.edu>
2022-03-11 13:29:08 -0700

    pulling

--- level.py
+++ level.py
@@ -1,20 +1,18 @@
 import pygame
 from tiles import Tile
-from settings import tile_size
+from settings import tile_size, screen_width
 from player import Player
-from power_up import Power_Up
 
 class Level:
     def __init__(self,level_data, surface):
         self.display_surface = surface
         self.setup_level(level_data)
         self.world_shift = 0
-        self.bg1 = pygame.image.load('assets\\graphics\\background.png')
+        # self.bg1 = pygame.image.load('assetsgraphics/background.png')
 
     def setup_level(self,layout):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
-        self.power_ups = pygame.sprite.Group()
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
                 if cell == 'X':
@@ -27,12 +25,22 @@ class Level:
                     y = row_index * tile_size
                     player_sprite = Player((x,y))
                     self.player.add(player_sprite)
-                if cell == 'O':
-                    x = col_index * tile_size
-                    y = row_index * tile_size
-                    power_sprite = Power_Up((x,y),30)
-                    self.power_ups.add(power_sprite)
             
+    def scrol_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width / 4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
     def horizonal_movment_collision(self):
         
         player = self.player.sprite
@@ -61,14 +69,12 @@ class Level:
                     player.direction.y = 0
 
     def run(self):
-        # self.tiles.update(self.world_shift)
+        self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
-
-        self.power_ups.update(self.world_shift)
-        self.power_ups.draw(self.display_surface)
 
         self.player.update()
         self.horizonal_movment_collision()
         self.virtical_movment_collision()
         self.player.draw(self.display_surface)
+        self.scrol_x()
         

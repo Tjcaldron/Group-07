30ab526a0f82325bdd9953333d1bc9d8636a5e27
Mosiah Hazen <haz17005@byui.edu>
2022-03-25 14:55:02 -0600

    doin it

--- sound.py
+++ sound.py
@@ -5,13 +5,10 @@ from pygame import mixer
 class Sound():
     def __init__(self):
         pygame.mixer.init()
-        
-        mixer.init.load()
-        
-        self.music = pygame.mixer.Sound("sound\background.wav")
+        self.music = pygame.mixer.Sound("sound/background.wav")
         self.jump = pygame.mixer.Sound("sound\Jump.wav")
         self.run = pygame.mixer.Sound("sound\Run.wav")
-        self.stop = pygame.mixer.Sound("sound\Stop.wav")
+        # self.stop = pygame.mixer.Sound("sound\Stop.wav")
         # self.fall = mixer.load.Sound("sound\Fall.wav") --- 1st import load statement for init method
         
 

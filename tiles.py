6d69ce87f73e71ca8ff083fc322960d1ce66b010
Tjcaldron <46572559+Tjcaldron@users.noreply.github.com>
2022-03-26 23:31:50 -0600

    main

--- tiles.py
+++ tiles.py
@@ -8,3 +8,8 @@ class Tile(pygame.sprite.Sprite):
 
     def update(self, x_shift):
         self.rect.x += x_shift
+
+##where enemy creation begins, it is part of the level tiles
+def enemies(self, layout, level_data):
+    enemy_layout = layout(level_data['enemies'])
+    self.enemy_sprites = self.create_tile_group(enemy_layout, 'enemies')

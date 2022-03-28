3bba30f754d2050ae1332ab89a44928b8c8b685e
Jane Orr <djaneorr@gmail.com>
2022-03-28 15:01:03 -0600

    changed map and tile properties

--- assets/maps/tilesets/bookpiles.tsx
+++ assets/maps/tilesets/bookpiles.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="bookpiles" tilewidth="16" tileheight="16" tilecount="4" columns="2">
- <image source="../../../../Group-07/assets/graphics/bookpiles.png" width="35" height="35"/>
+<tileset version="1.8" tiledversion="1.8.1" name="bookpiles" tilewidth="32" tileheight="32" tilecount="4" columns="1">
+ <grid orientation="orthogonal" width="64" height="64"/>
+ <image source="../../graphics/bookpiles.png" width="35" height="35"/>
 </tileset>

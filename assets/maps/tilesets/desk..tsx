764488600451d2bc331c0dc09726d41c3c119aa8
Tjcaldron <46572559+Tjcaldron@users.noreply.github.com>
2022-03-28 17:30:44 -0600

    main

---
+++ assets/maps/tilesets/desk..tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="desk" tilewidth="16" tileheight="16" tilecount="4" columns="2">
+ <image source="../../graphics/desk.png" width="35" height="35"/>
+</tileset>
